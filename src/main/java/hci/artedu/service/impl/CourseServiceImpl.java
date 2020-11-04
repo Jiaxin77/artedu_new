32e20a4cceb2bd8afb341ece8a1136e8d101713b
Jiaxin77 <905284679@qq.com>
2020-11-04 15:54:13 +0800

    增加了课程mapper

--- src/main/java/hci/artedu/service/impl/CourseServiceImpl.java
+++ src/main/java/hci/artedu/service/impl/CourseServiceImpl.java
@@ -1,22 +1,106 @@
-package hci.artedu.service.impl;
-
-import hci.artedu.service.CourseService;
-
-/**
- * @ClassName CourseServiceImpl
- * @Description TODO
- * @Author jiaxin
- * @Date 2020/11/2 9:33 下午
- * @Version 1.0
- **/
-public class CourseServiceImpl implements CourseService {
-    /**
-     * @Author jiaxin
-     * @Description 获取知识点//TODO
-     * @Date 9:39 下午 2020/11/2
-     * @Param
-     * @return
-     **/
-
-
-}
+//package hci.artedu.service.impl;
+//
+//import hci.artedu.common.ServerResponse;
+//import hci.artedu.dao.CourseMapper;
+//import hci.artedu.pojo.Course;
+//import hci.artedu.service.CourseService;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//
+///**
+// * @ClassName CourseServiceImpl
+// * @Description TODO
+// * @Author jiaxin
+// * @Date 2020/11/2 9:33 下午
+// * @Version 1.0
+// **/
+//@Service
+//public class CourseServiceImpl implements CourseService {
+//    /**
+//     * @Author jiaxin
+//     * @Description 获取课程信息以及知识点//TODO
+//     * @Date 9:39 下午 2020/11/2
+//     * @Param
+//     * @return
+//     **/
+//
+//    CourseMapper courseMapper = new
+//
+//    public ServerResponse<HashMap<String,Object>> getCourseInfo()
+//    {
+//        /*
+//         * @Author jiaxin
+//         * @Description 获取课程信息//TODO
+//         * @Date 11:33 上午 2020/11/4
+//         * @Param []
+//         * @return hci.artedu.common.ServerResponse<java.util.HashMap<java.lang.String,java.lang.Object>>
+//         **/
+//
+//        int courseId = 1;
+//        Course course =
+//
+//
+//
+//
+//    }
+//
+//    public ServerResponse<ArrayList> getKnowledgePointList()
+//    {
+//        /**
+//         * @Author jiaxin
+//         * @Description 获取知识点列表//TODO
+//         * @Date 2:52 下午 2020/11/4
+//         * @Param []
+//         * @return hci.artedu.common.ServerResponse<java.util.ArrayList>
+//         **/
+//
+//
+//    }
+//
+//    public ServerResponse<HashMap<String,Object>> getKnowledgePointInfo(int pointId)
+//    {
+//        /**
+//         * @Author jiaxin
+//         * @Description 获取某知识点内容//TODO
+//         * @Date 3:04 下午 2020/11/4
+//         * @Param [pointId]
+//         * @return hci.artedu.common.ServerResponse<java.util.HashMap<java.lang.String,java.lang.Object>>
+//         **/
+//
+//    }
+//
+//    public ServerResponse<HashMap<String,Object>> getUserPoint(int userId)
+//    {
+//        /**
+//         * @Author jiaxin
+//         * @Description 获取某用户的知识点进度//TODO
+//         * @Date 3:05 下午 2020/11/4
+//         * @Param [userId]
+//         * @return hci.artedu.common.ServerResponse<java.util.HashMap<java.lang.String,java.lang.Object>>
+//         **/
+//
+//    }
+//
+//    public ServerResponse<ArrayList> getPointEpt(int pointId)
+//    {
+//        /**
+//         * @Author jiaxin
+//         * @Description 获取知识点对应的实验列表以及每个实验的知识点列表//TODO
+//         * @Date 3:43 下午 2020/11/4
+//         * @Param [pointId]
+//         * @return hci.artedu.common.ServerResponse<java.util.ArrayList>
+//         **/
+//
+//    }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//}
